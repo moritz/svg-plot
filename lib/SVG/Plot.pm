ed3541c95948f3bb34c841f2d05f57ef1240da68
Moritz Lenz <moritz@faui2k3.org>
2009-08-14 21:22:16 +0200

    move plotting routine to a module

---
+++ lib/SVG/Plot.pm
@@ -0,0 +1,67 @@
+
+class SVG::Plot {
+    has $.height = 200;
+    has $.width  = 300;
+
+    method plot(@data, :$full = True) {
+        my $max_x = 0;
+        my $max_y = 0;
+
+        my @svg_d = gather {
+            for @data.kv -> $k, $v {
+                take 'rect' => [
+                    :y(0),
+                    :x($k * 40),
+                    :width(35),
+                    :height($v),
+                    :style<fill:blue>,
+                ];
+                $max_x = $k * 40;
+                $max_y max= $v;
+            }
+        }
+
+
+        my @transformation = (
+                $.width / $max_x,   # scaling in x direction,
+                0,                  # x-y skew
+                0,                  # y-x skew
+                -$.height / $max_y, # scaling in y direction,
+                                    # negative, since SVG defines the
+                                    # positive y axis downwards
+                0,                  # translation x
+                $.height,           # translation y
+        );
+        my $trafo = 'matrix(' ~ @transformation.join(',') ~ ')';
+
+        my @svg = 'g' => [
+                :transform($trafo),
+                @svg_d,
+                ],
+                'line' => [
+                    :x1(0),
+                    :y1($.height),
+                    :x2($.width),
+                    :y2($.height),
+                    :style('stroke:black; stroke-width: 2'),
+                ],
+                'line' => [
+                    :x1(0),
+                    :y1(0),
+                    :x2(0),
+                    :y2($.height),
+                    :style('stroke:black; stroke-width: 2'),
+                ];
+
+        return $full
+            ??  
+                :svg([
+                        :width(320), :height(220),
+                        'xmlns:svg' => 'http://www.w3.org/2000/svg',
+                        @svg
+                ])
+            !! @svg;
+    }
+}
+
+# vim: ft=perl6
--- plot.pl
+++ plot.pl
@@ -1,70 +1,12 @@
 use v6;
 
-BEGIN { @*INC.push: '../svg/lib' }
+BEGIN { @*INC.push: '../svg/lib', 'lib' }
 
 use SVG;
-
-my $width = 300;
-my $height = 200;
-
-
-sub data-to-svg(@data, :$width, :$height) {
-    my $max_x = 0;
-    my $max_y = 0;
-
-    my @svg_d = gather {
-        for @data.kv -> $k, $v {
-            take 'rect' => [
-                :y(0),
-                :x($k * 40),
-                :width(35),
-                :height($v),
-                :style<fill:blue>,
-            ];
-            $max_x = $k * 40;
-            $max_y max= $v;
-        }
-    }
-
-
-    my @transformation = (
-            $width / $max_x,    # scaling in x direction,
-            0,                  # x-y skew
-            0,                  # y-x skew
-            -$height / $max_y,  # scaling in y direction,
-                                # negative, since SVG defines the positive
-                                # y axis downwards
-            0,                  # translation x
-            $height,            # translation y
-    );
-
-    my $trafo = 'matrix(' ~ @transformation.join(',') ~ ')';
-    return 'g' => [
-        :transform($trafo),
-        @svg_d,
-    ];
-}
+use SVG::Plot;
 
 my @data = 5, 6, 4, 3, 8, 12, 0, 0, 3, 7;
-my $svg = :svg([
-    :wdith(320), :height(220),
-    'xmlns:svg' => 'http://www.w3.org/2000/svg',
-    data-to-svg(@data, :width(300), :height(200)),
-    'line' => [
-        :x1(0),
-        :y1($height),
-        :x2($width),
-        :y2($height),
-        :style('stroke:black; stroke-width: 2'),
-    ],
-    'line' => [
-        :x1(0),
-        :y1(0),
-        :x2(0),
-        :y2($height),
-        :style('stroke:black; stroke-width: 2'),
-    ],
-]);
+my $svg = SVG::Plot.new(width => 300, height => 200).plot(@data);
 
 say SVG.serialize($svg);
 
