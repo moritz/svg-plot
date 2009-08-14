0a79fd7438fe649ca21047239f0e15bdad7b56b6
Moritz Lenz <moritz@faui2k3.org>
2009-08-14 21:31:32 +0200

    add an example script
    This is a simple parser for an Apache access.log in "combined" format that
    calculates visitors per day, and plots it.

---
+++ examples/parse-access-log.pl
@@ -0,0 +1,94 @@
+use v6;
+BEGIN {
+    @*INC.push: '../lib', '../../svg/lib',
+                'lib', '../svg/lib';
+}
+use SVG;
+use SVG::Plot;
+
+grammar AccessLog {
+    token TOP {
+        ^ <ip>
+          \s <dummy>
+          \s <dummy>
+          \s <date>
+          \s <request>
+          \s <status>
+          \s <size>
+          \s <referrer>
+          \s <user_agent>
+          $
+    }
+    regex ip {
+        | <ipv4>
+        | <ipv6>
+    }
+
+    token ipv4 {
+             \d ** 1..3
+        [ \. \d ** 1..3 ] ** 3
+    }
+
+    token ipv6 {
+        <[0..9 :]>+     # need a better regex 
+    }
+
+    token dummy { '-' }
+
+    token date {
+        '[' ~  ']'
+            [
+            ( \d\d ) '/' ( \w\w\w ) '/' ( \d\d\d\d )
+            <-[ \] ]>+
+            ]
+    }
+
+    token request { \" <-[ " ]>* \" }
+
+    token status { <[0..9]>+ }
+
+    token size { <[0..9]>+ | <dummy> }
+
+    token referrer { \" <-[ " ]>* \" }
+
+    token user_agent { 
+        \" ~ \" 
+        [ 
+            | <-[ " ]>
+            | \\ \"
+        ]* 
+    }
+}
+
+
+my $f = open('access2.log');
+
+my $line;
+
+
+my %months = <Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dez> Z 1..12;
+
+my $prev_date;
+my %user;
+my @visitor_per_day;
+
+while $line = $f.get {
+    AccessLog.parse($line) or next;
+    my $date = sprintf '%04d%02d%02d', $<date>[2], 
+                       %months{$<date>[1]}, $<date>[0];
+    $prev_date //= $date;
+
+    if $prev_date ne $date {
+        push @visitor_per_day, +%user;
+        %user = ();
+        $prev_date = $date;
+    }
+
+
+    %user{$<ip> ~ $<user_agent>}++;
+}
+
+my $svg = SVG::Plot.new(:width(600), :height(400)).plot(@visitor_per_day);
+say SVG.serialize($svg);
+
+# vim: ft=perl6
