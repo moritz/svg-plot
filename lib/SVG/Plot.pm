01767eea8a4b1ce2fd16cfc877f6a73fbedec658
Moritz Lenz <moritz@faui2k3.org>
2009-08-20 23:39:38 +0200

    outdent class definition

--- lib/SVG/Plot.pm
+++ lib/SVG/Plot.pm
@@ -1,130 +1,128 @@
+class SVG::Plot;
+has $.height            = 200;
+has $.width             = 300;
+has $.fill-width        = 0.80;
+has $.label-font-size   = 14;
+has $.plot-width        = $.width  * 0.80;
+has $.plot-height       = $.height * 0.65;
 
-class SVG::Plot {
-    has $.height            = 200;
-    has $.width             = 300;
-    has $.fill-width        = 0.80;
-    has $.label-font-size   = 14;
-    has $.plot-width        = $.width  * 0.80;
-    has $.plot-height       = $.height * 0.65;
+has &.y-tick-step       = -> $max_y {
+    10 ** floor(log10($max_y)) / 5
+}
 
-    has &.y-tick-step       = -> $max_y {
-        10 ** floor(log10($max_y)) / 5
-    }
+has $.max-x-labels      = $.plot-width / (1.5 * $.label-font-size);
 
-    has $.max-x-labels      = $.plot-width / (1.5 * $.label-font-size);
+has $.label-spacing     = ($.height - $.plot-height) / 20;
 
-    has $.label-spacing     = ($.height - $.plot-height) / 20;
+has @.values is rw;
+has @.labels is rw = @.values.keys;
+has @.links  is rw;
 
-    has @.values is rw;
-    has @.labels is rw = @.values.keys;
-    has @.links  is rw;
+method plot(:$full = True) {
+    my $label-skip = ceiling(@.values / $.max-x-labels);
+    my $max_x = +@.values;
+    my $max_y = [max] @.values;
 
-    method plot(:$full = True) {
-        my $label-skip = ceiling(@.values / $.max-x-labels);
-        my $max_x = +@.values;
-        my $max_y = [max] @.values;
+    my $step_x = $.plot-width  / $max_x;
+    my $step_y = $.plot-height / $max_y;
 
-        my $step_x = $.plot-width  / $max_x;
-        my $step_y = $.plot-height / $max_y;
+    my @svg_d = gather {
+        for @.values.keys Z @.values.values Z @.labels -> $k, $v, $l {
+            my $p = 'rect' => [
+                :y(-$v * $step_y),
+                :x($k * $step_x),
+                :width($.fill-width * $step_x),
+                :height($v * $step_y),
+                :style<fill:blue>,
+            ];
+            take self!linkify($k, $p);
 
-        my @svg_d = gather {
-            for @.values.keys Z @.values.values Z @.labels -> $k, $v, $l {
-                my $p = 'rect' => [
-                    :y(-$v * $step_y),
-                    :x($k * $step_x),
-                    :width($.fill-width * $step_x),
-                    :height($v * $step_y),
-                    :style<fill:blue>,
+            if $k !% $label-skip {
+                # note that the rotation is applied first,
+                # so we have to  transform our 
+                # coordinates first: 
+                # x -> - y 
+                # y ->   x
+                my $t = 'text' => [
+                    :transform('rotate(90)'),
+                    :y((-$k - 0.5 * $.fill-width) * $step_x),
+                    :x($.label-spacing),
+                    :font-size($.label-font-size),
+                    :dominant-baseline<middle>,
+                    ~$l,
                 ];
-                take self!linkify($k, $p);
-
-                if $k !% $label-skip {
-                    # note that the rotation is applied first,
-                    # so we have to  transform our 
-                    # coordinates first: 
-                    # x -> - y 
-                    # y ->   x
-                    my $t = 'text' => [
-                        :transform('rotate(90)'),
-                        :y((-$k - 0.5 * $.fill-width) * $step_x),
-                        :x($.label-spacing),
-                        :font-size($.label-font-size),
-                        :dominant-baseline<middle>,
-                        ~$l,
-                    ];
-                    take self!linkify($k, $t);
-                }
-
+                take self!linkify($k, $t);
             }
-            self!y-ticks($max_y, $step_y);
+
         }
+        self!y-ticks($max_y, $step_y);
+    }
 
-        my $x-trafo = 0.8 * ($.width - $.plot-width);
-        my $y-trafo = $.plot-height + 0.3 * ($.height - $.plot-height);
-        my $trafo = "translate($x-trafo,$y-trafo)";
+    my $x-trafo = 0.8 * ($.width - $.plot-width);
+    my $y-trafo = $.plot-height + 0.3 * ($.height - $.plot-height);
+    my $trafo = "translate($x-trafo,$y-trafo)";
 
-        my @svg = 'g' => [
-            :transform($trafo),
-            @svg_d,
-            'line' => [
-                :x1(0),
-                :y1(0),
-                :x2($.plot-width),
-                :y2(0),
-                :style('stroke:black; stroke-width: 2'),
-            ],
-            'line' => [
-                :x1(0),
-                :y1(0),
-                :x2(0),
-                :y2(-$.plot-height),
-                :style('stroke:black; stroke-width: 2'),
-            ],
+    my @svg = 'g' => [
+        :transform($trafo),
+        @svg_d,
+        'line' => [
+            :x1(0),
+            :y1(0),
+            :x2($.plot-width),
+            :y2(0),
+            :style('stroke:black; stroke-width: 2'),
+        ],
+        'line' => [
+            :x1(0),
+            :y1(0),
+            :x2(0),
+            :y2(-$.plot-height),
+            :style('stroke:black; stroke-width: 2'),
+        ],
+    ];
+
+    return $full
+        ??
+            :svg([
+                    :width($.width), :height($.height),
+                    'xmlns' => 'http://www.w3.org/2000/svg',
+                    'xmlns:svg' => 'http://www.w3.org/2000/svg',
+                    'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
+                    @svg
+            ])
+        !! @svg;
+}
+
+method !y-ticks($max_y, $scale_y) {
+    my $step = (&.y-tick-step).($max_y);
+    loop (my $y = 0; $y <= $max_y; $y += $step) {
+        take 'line' => [
+            :x1(-$.label-spacing / 2),
+            :x2( $.label-spacing / 2),
+            :y1(-$y * $scale_y),
+            :y2(-$y * $scale_y),
+            :style('stroke:black; stroke-width: 1'),
         ];
-
-        return $full
-            ??  
-                :svg([
-                        :width($.width), :height($.height),
-                        'xmlns' => 'http://www.w3.org/2000/svg',
-                        'xmlns:svg' => 'http://www.w3.org/2000/svg',
-                        'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
-                        @svg
-                ])
-            !! @svg;
+        take 'text' => [
+            :x(- 1.5 * $.label-spacing),
+            :y(-$y * $scale_y),
+            :font-size($.label-font-size),
+            :text-anchor<end>,
+            :dominant-baseline<middle>,
+            ~ $y,
+        ];
     }
+}
 
-    method !y-ticks($max_y, $scale_y) {
-        my $step = (&.y-tick-step).($max_y);
-        loop (my $y = 0; $y <= $max_y; $y += $step) {
-            take 'line' => [
-                :x1(-$.label-spacing / 2),
-                :x2( $.label-spacing / 2),
-                :y1(-$y * $scale_y),
-                :y2(-$y * $scale_y),
-                :style('stroke:black; stroke-width: 1'),
-            ];
-            take 'text' => [
-                :x(- 1.5 * $.label-spacing),
-                :y(-$y * $scale_y),
-                :font-size($.label-font-size),
-                :text-anchor<end>,
-                :dominant-baseline<middle>,
-                ~ $y,
-            ];
-        }
-    }
-
-    method !linkify($key, $thing) {
-        my $link = @.links[$key];
-        defined($link)
-            ?? ('a' => [
-                    'xlink:href' => $link,
-                    :target<_top>,
-                    $thing
-                ])
-            !! $thing;
-    }
+method !linkify($key, $thing) {
+    my $link = @.links[$key];
+    defined($link)
+        ?? ('a' => [
+                'xlink:href' => $link,
+                :target<_top>,
+                $thing
+            ])
+        !! $thing;
 }
 
 =begin Pod
