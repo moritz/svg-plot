d29750991760987f4372bb883b6ddf327e570afa
Moritz Lenz <moritz@faui2k3.org>
2010-06-19 15:48:51 +0200

    first shot at xy-points plot

---
+++ examples/circle.pl
@@ -0,0 +1,15 @@
+use SVG;
+use SVG::Plot;
+
+my $points = 50;
+my @x  = (0..$points).map: { sin(2 * pi * $_ / $points) };
+my @d1 = (0..$points).map: { 2 * cos($_ / $points) };
+my @d2 = (0..$points).map: { cos($_ / $points) };
+my $svg = SVG::Plot.new(
+    width => 400,
+    height => 250,
+    :@x,
+    values => ([@d1], [@d2]),
+    title  => 'sin(x/10), cos(x/10)',
+).plot(:xy-points);
+say SVG.serialize($svg);
