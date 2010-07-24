db841bbfab9833f182cf26ffcf3076da77e95526
Moritz Lenz <moritz@faui2k3.org>
2010-07-24 10:09:56 +0200

    Data::Marker. Not yet used.

---
+++ lib/SVG/Plot/Data/Marker.pm
@@ -0,0 +1,11 @@
+class SVG::Plot::Data::Marker;
+
+has $.brush;            # TODO: default value
+has $.color = 'black';  # TODO: object
+has $.inside_color = 'black';
+has Numeric $.key;
+has Numeric $.value;
+has Numeric $.key2;
+has Numeric $.value2;
+
+# vim: ft=perl6
