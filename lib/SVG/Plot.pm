5790dce4ecd6f61eadcb478f1a08f90cfa7d5312
thundergnat <thundergnat@comcast.net>
2011-07-15 13:53:35 -0700

    Added y-axis attribute to set the y axis at something other than the minimum if desired.

--- lib/SVG/Plot.pm
+++ lib/SVG/Plot.pm
@@ -34,6 +34,8 @@ has $.label-spacing     = ($.height - $.plot-height) / 20;
 
 has @.colors = <#3333ff #ffdd66 #aa2222 #228844 #eebb00 #8822bb>;
 
+has $.y-axis            = 0;
+
 multi method plot(:$full = True, :$stacked-bars!) {
 
     my $label-skip = ceiling(@.values[0] / $.max-x-labels);
@@ -87,7 +89,7 @@ multi method plot(:$full = True, :$bars!) {
     # the minimum is only interesting if it's smaller than 0.
     # if all the values are non-negative, the bars should still start
     # at 0
-    my $min_y      = ([min] @.values.map: { [min] @($_) }) min 0;
+    my $min_y      = ([min] $.y-axis, @.values.map: { [min] @($_) }) min 0;
 
     my $datasets   = +@.values;
 
@@ -132,7 +134,7 @@ multi method plot(:$full = True, :$points!) {
     my $label-skip = ceiling(@.values[0] / $.max-x-labels);
     my $max_x      = @.values[0].elems;
     my $max_y      = [max] @.values.map: { [max] @($_) };
-    my $min_y      = [min] @.values.map: { [min] @($_) };
+    my $min_y      = [min] $.y-axis, @.values.map: { [min] @($_) };
     my $datasets   = +@.values;
 
     my $step_x     = $.plot-width  / $max_x;
@@ -170,7 +172,7 @@ multi method plot(:$full = True, :$points-with-errors!,
     my $label-skip = ceiling(@.values[0] / $.max-x-labels);
     my $max_x      = @.values[0].elems;
     my $max_y      = [max] @upper.map: { [max] @($_) };
-    my $min_y      = [min] @lower.map: { [min] @($_) };
+    my $min_y      = [min] $.y-axis, @lower.map: { [min] @($_) };
     my $datasets   = +@.values;
 
     my $step_x     = $.plot-width  / $max_x;
@@ -234,7 +236,7 @@ multi method plot(:$full = True, :$xy-points!) {
     my $min_x      = [min] @.x;
 
     my $max_y      = [max] @.values.map: { [max] @($_) };
-    my $min_y      = [min] @.values.map: { [min] @($_) };
+    my $min_y      = [min] $.y-axis, @.values.map: { [min] @($_) };
 
     my $datasets   = +@.values;
 
@@ -280,7 +282,7 @@ multi method plot(:$full = True, :$xy-lines!) {
     }
 
     my $max_y      = [max] @.values.map: { [max] @($_) };
-    my $min_y      = [min] @.values.map: { [min] @($_) };
+    my $min_y      = [min] $.y-axis, @.values.map: { [min] @($_) };
 
     if $max_y == $min_y {
         die "There's just one y value ($max_x), refusing to plot\n";
@@ -329,7 +331,7 @@ multi method plot(:$full = True, :$lines!) {
     my $label-skip = ceiling(@.values[0] / $.max-x-labels);
     my $max_x      = @.values[0].elems;
     my $max_y      = [max] @.values.map: { [max] @($_) };
-    my $min_y      = [min] @.values.map: { [min] @($_) };
+    my $min_y      = [min] $.y-axis, @.values.map: { [min] @($_) };
     my $datasets   = +@.values;
 
     my $step_x     = $.plot-width  / $max_x;
