99652be9524c6ac19060771928f5c52a42b11e70
Moritz Lenz <moritz@faui2k3.org>
2015-10-11 22:16:46 +0200

    Change "for" back to "map"
    for "for" is no more.

--- examples/circle.pl
+++ examples/circle.pl
@@ -2,13 +2,13 @@ use SVG;
 use SVG::Plot;
 
 my $points = 50;
-my @x  = (0..$points).for: { sin(2 * pi * $_ / $points) };
-my @d1 = (0..$points).for: { 2 * cos(2 * pi * $_ / $points) };
-my @d2 = (0..$points).for: { cos(2 * pi * $_ / $points) };
+my @x  = (0..$points).map: { sin(2 * pi * $_ / $points) };
+my @d1 = (0..$points).map: { 2 * cos(2 * pi * $_ / $points) };
+my @d2 = (0..$points).map: { cos(2 * pi * $_ / $points) };
 say SVG.serialize: SVG::Plot.new(
     width => 400,
     height => 250,
     :@x,
-    values => ([@d1], [@d2]),
+    values => (@d1, @d2),
     title  => 'sin(x/10), cos(x/10)',
 ).plot(:xy-lines);
